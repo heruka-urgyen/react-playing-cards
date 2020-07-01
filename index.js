77ebf61d58c52a03188692d5aee201d41f92ad50
urgyen <imalyanov@gmail.com>
2020-07-01 18:01:18 -0500

    add react card component

---
+++ index.js
@@ -0,0 +1,492 @@
+import React from "react"
+
+import b from "./images/back.svg"
+
+// two color normal face
+import tc2c from "./images/2color/2C.svg"
+import tc3c from "./images/2color/3C.svg"
+import tc4c from "./images/2color/4C.svg"
+import tc5c from "./images/2color/5C.svg"
+import tc6c from "./images/2color/6C.svg"
+import tc7c from "./images/2color/7C.svg"
+import tc8c from "./images/2color/8C.svg"
+import tc9c from "./images/2color/9C.svg"
+import tcTc from "./images/2color/TC.svg"
+import tcJc from "./images/2color/JC.svg"
+import tcQc from "./images/2color/QC.svg"
+import tcKc from "./images/2color/KC.svg"
+import tcAc from "./images/2color/AC.svg"
+
+import tc2d from "./images/2color/2D.svg"
+import tc3d from "./images/2color/3D.svg"
+import tc4d from "./images/2color/4D.svg"
+import tc5d from "./images/2color/5D.svg"
+import tc6d from "./images/2color/6D.svg"
+import tc7d from "./images/2color/7D.svg"
+import tc8d from "./images/2color/8D.svg"
+import tc9d from "./images/2color/9D.svg"
+import tcTd from "./images/2color/TD.svg"
+import tcJd from "./images/2color/JD.svg"
+import tcQd from "./images/2color/QD.svg"
+import tcKd from "./images/2color/KD.svg"
+import tcAd from "./images/2color/AD.svg"
+
+import tc2h from "./images/2color/2H.svg"
+import tc3h from "./images/2color/3H.svg"
+import tc4h from "./images/2color/4H.svg"
+import tc5h from "./images/2color/5H.svg"
+import tc6h from "./images/2color/6H.svg"
+import tc7h from "./images/2color/7H.svg"
+import tc8h from "./images/2color/8H.svg"
+import tc9h from "./images/2color/9H.svg"
+import tcTh from "./images/2color/TH.svg"
+import tcJh from "./images/2color/JH.svg"
+import tcQh from "./images/2color/QH.svg"
+import tcKh from "./images/2color/KH.svg"
+import tcAh from "./images/2color/AH.svg"
+
+import tc2s from "./images/2color/2S.svg"
+import tc3s from "./images/2color/3S.svg"
+import tc4s from "./images/2color/4S.svg"
+import tc5s from "./images/2color/5S.svg"
+import tc6s from "./images/2color/6S.svg"
+import tc7s from "./images/2color/7S.svg"
+import tc8s from "./images/2color/8S.svg"
+import tc9s from "./images/2color/9S.svg"
+import tcTs from "./images/2color/TS.svg"
+import tcJs from "./images/2color/JS.svg"
+import tcQs from "./images/2color/QS.svg"
+import tcKs from "./images/2color/KS.svg"
+import tcAs from "./images/2color/AS.svg"
+
+// two color big face
+import tcbf2c from "./images/2color-bf/2C.svg"
+import tcbf3c from "./images/2color-bf/3C.svg"
+import tcbf4c from "./images/2color-bf/4C.svg"
+import tcbf5c from "./images/2color-bf/5C.svg"
+import tcbf6c from "./images/2color-bf/6C.svg"
+import tcbf7c from "./images/2color-bf/7C.svg"
+import tcbf8c from "./images/2color-bf/8C.svg"
+import tcbf9c from "./images/2color-bf/9C.svg"
+import tcbfTc from "./images/2color-bf/TC.svg"
+import tcbfJc from "./images/2color-bf/JC.svg"
+import tcbfQc from "./images/2color-bf/QC.svg"
+import tcbfKc from "./images/2color-bf/KC.svg"
+import tcbfAc from "./images/2color-bf/AC.svg"
+
+import tcbf2d from "./images/2color-bf/2D.svg"
+import tcbf3d from "./images/2color-bf/3D.svg"
+import tcbf4d from "./images/2color-bf/4D.svg"
+import tcbf5d from "./images/2color-bf/5D.svg"
+import tcbf6d from "./images/2color-bf/6D.svg"
+import tcbf7d from "./images/2color-bf/7D.svg"
+import tcbf8d from "./images/2color-bf/8D.svg"
+import tcbf9d from "./images/2color-bf/9D.svg"
+import tcbfTd from "./images/2color-bf/TD.svg"
+import tcbfJd from "./images/2color-bf/JD.svg"
+import tcbfQd from "./images/2color-bf/QD.svg"
+import tcbfKd from "./images/2color-bf/KD.svg"
+import tcbfAd from "./images/2color-bf/AD.svg"
+
+import tcbf2h from "./images/2color-bf/2H.svg"
+import tcbf3h from "./images/2color-bf/3H.svg"
+import tcbf4h from "./images/2color-bf/4H.svg"
+import tcbf5h from "./images/2color-bf/5H.svg"
+import tcbf6h from "./images/2color-bf/6H.svg"
+import tcbf7h from "./images/2color-bf/7H.svg"
+import tcbf8h from "./images/2color-bf/8H.svg"
+import tcbf9h from "./images/2color-bf/9H.svg"
+import tcbfTh from "./images/2color-bf/TH.svg"
+import tcbfJh from "./images/2color-bf/JH.svg"
+import tcbfQh from "./images/2color-bf/QH.svg"
+import tcbfKh from "./images/2color-bf/KH.svg"
+import tcbfAh from "./images/2color-bf/AH.svg"
+
+import tcbf2s from "./images/2color-bf/2S.svg"
+import tcbf3s from "./images/2color-bf/3S.svg"
+import tcbf4s from "./images/2color-bf/4S.svg"
+import tcbf5s from "./images/2color-bf/5S.svg"
+import tcbf6s from "./images/2color-bf/6S.svg"
+import tcbf7s from "./images/2color-bf/7S.svg"
+import tcbf8s from "./images/2color-bf/8S.svg"
+import tcbf9s from "./images/2color-bf/9S.svg"
+import tcbfTs from "./images/2color-bf/TS.svg"
+import tcbfJs from "./images/2color-bf/JS.svg"
+import tcbfQs from "./images/2color-bf/QS.svg"
+import tcbfKs from "./images/2color-bf/KS.svg"
+import tcbfAs from "./images/2color-bf/AS.svg"
+
+// four color
+import fc2c from "./images/4color/2C.svg"
+import fc3c from "./images/4color/3C.svg"
+import fc4c from "./images/4color/4C.svg"
+import fc5c from "./images/4color/5C.svg"
+import fc6c from "./images/4color/6C.svg"
+import fc7c from "./images/4color/7C.svg"
+import fc8c from "./images/4color/8C.svg"
+import fc9c from "./images/4color/9C.svg"
+import fcTc from "./images/4color/TC.svg"
+import fcJc from "./images/4color/JC.svg"
+import fcQc from "./images/4color/QC.svg"
+import fcKc from "./images/4color/KC.svg"
+import fcAc from "./images/4color/AC.svg"
+
+import fc2d from "./images/4color/2D.svg"
+import fc3d from "./images/4color/3D.svg"
+import fc4d from "./images/4color/4D.svg"
+import fc5d from "./images/4color/5D.svg"
+import fc6d from "./images/4color/6D.svg"
+import fc7d from "./images/4color/7D.svg"
+import fc8d from "./images/4color/8D.svg"
+import fc9d from "./images/4color/9D.svg"
+import fcTd from "./images/4color/TD.svg"
+import fcJd from "./images/4color/JD.svg"
+import fcQd from "./images/4color/QD.svg"
+import fcKd from "./images/4color/KD.svg"
+import fcAd from "./images/4color/AD.svg"
+
+import fc2h from "./images/4color/2H.svg"
+import fc3h from "./images/4color/3H.svg"
+import fc4h from "./images/4color/4H.svg"
+import fc5h from "./images/4color/5H.svg"
+import fc6h from "./images/4color/6H.svg"
+import fc7h from "./images/4color/7H.svg"
+import fc8h from "./images/4color/8H.svg"
+import fc9h from "./images/4color/9H.svg"
+import fcTh from "./images/4color/TH.svg"
+import fcJh from "./images/4color/JH.svg"
+import fcQh from "./images/4color/QH.svg"
+import fcKh from "./images/4color/KH.svg"
+import fcAh from "./images/4color/AH.svg"
+
+import fc2s from "./images/4color/2S.svg"
+import fc3s from "./images/4color/3S.svg"
+import fc4s from "./images/4color/4S.svg"
+import fc5s from "./images/4color/5S.svg"
+import fc6s from "./images/4color/6S.svg"
+import fc7s from "./images/4color/7S.svg"
+import fc8s from "./images/4color/8S.svg"
+import fc9s from "./images/4color/9S.svg"
+import fcTs from "./images/4color/TS.svg"
+import fcJs from "./images/4color/JS.svg"
+import fcQs from "./images/4color/QS.svg"
+import fcKs from "./images/4color/KS.svg"
+import fcAs from "./images/4color/AS.svg"
+
+// four color big face
+import fcbf2c from "./images/4color-bf/2C.svg"
+import fcbf3c from "./images/4color-bf/3C.svg"
+import fcbf4c from "./images/4color-bf/4C.svg"
+import fcbf5c from "./images/4color-bf/5C.svg"
+import fcbf6c from "./images/4color-bf/6C.svg"
+import fcbf7c from "./images/4color-bf/7C.svg"
+import fcbf8c from "./images/4color-bf/8C.svg"
+import fcbf9c from "./images/4color-bf/9C.svg"
+import fcbfTc from "./images/4color-bf/TC.svg"
+import fcbfJc from "./images/4color-bf/JC.svg"
+import fcbfQc from "./images/4color-bf/QC.svg"
+import fcbfKc from "./images/4color-bf/KC.svg"
+import fcbfAc from "./images/4color-bf/AC.svg"
+
+import fcbf2d from "./images/4color-bf/2D.svg"
+import fcbf3d from "./images/4color-bf/3D.svg"
+import fcbf4d from "./images/4color-bf/4D.svg"
+import fcbf5d from "./images/4color-bf/5D.svg"
+import fcbf6d from "./images/4color-bf/6D.svg"
+import fcbf7d from "./images/4color-bf/7D.svg"
+import fcbf8d from "./images/4color-bf/8D.svg"
+import fcbf9d from "./images/4color-bf/9D.svg"
+import fcbfTd from "./images/4color-bf/TD.svg"
+import fcbfJd from "./images/4color-bf/JD.svg"
+import fcbfQd from "./images/4color-bf/QD.svg"
+import fcbfKd from "./images/4color-bf/KD.svg"
+import fcbfAd from "./images/4color-bf/AD.svg"
+
+import fcbf2h from "./images/4color-bf/2H.svg"
+import fcbf3h from "./images/4color-bf/3H.svg"
+import fcbf4h from "./images/4color-bf/4H.svg"
+import fcbf5h from "./images/4color-bf/5H.svg"
+import fcbf6h from "./images/4color-bf/6H.svg"
+import fcbf7h from "./images/4color-bf/7H.svg"
+import fcbf8h from "./images/4color-bf/8H.svg"
+import fcbf9h from "./images/4color-bf/9H.svg"
+import fcbfTh from "./images/4color-bf/TH.svg"
+import fcbfJh from "./images/4color-bf/JH.svg"
+import fcbfQh from "./images/4color-bf/QH.svg"
+import fcbfKh from "./images/4color-bf/KH.svg"
+import fcbfAh from "./images/4color-bf/AH.svg"
+
+import fcbf2s from "./images/4color-bf/2S.svg"
+import fcbf3s from "./images/4color-bf/3S.svg"
+import fcbf4s from "./images/4color-bf/4S.svg"
+import fcbf5s from "./images/4color-bf/5S.svg"
+import fcbf6s from "./images/4color-bf/6S.svg"
+import fcbf7s from "./images/4color-bf/7S.svg"
+import fcbf8s from "./images/4color-bf/8S.svg"
+import fcbf9s from "./images/4color-bf/9S.svg"
+import fcbfTs from "./images/4color-bf/TS.svg"
+import fcbfJs from "./images/4color-bf/JS.svg"
+import fcbfQs from "./images/4color-bf/QS.svg"
+import fcbfKs from "./images/4color-bf/KS.svg"
+import fcbfAs from "./images/4color-bf/AS.svg"
+
+const cardsTwoColor = {
+  '2c': tc2c,
+  '3c': tc3c,
+  '4c': tc4c,
+  '5c': tc5c,
+  '6c': tc6c,
+  '7c': tc7c,
+  '8c': tc8c,
+  '9c': tc9c,
+  'Tc': tcTc,
+  'Jc': tcJc,
+  'Qc': tcQc,
+  'Kc': tcKc,
+  'Ac': tcAc,
+
+  '2d': tc2d,
+  '3d': tc3d,
+  '4d': tc4d,
+  '5d': tc5d,
+  '6d': tc6d,
+  '7d': tc7d,
+  '8d': tc8d,
+  '9d': tc9d,
+  'Td': tcTd,
+  'Jd': tcJd,
+  'Qd': tcQd,
+  'Kd': tcKd,
+  'Ad': tcAd,
+
+  '2h': tc2h,
+  '3h': tc3h,
+  '4h': tc4h,
+  '5h': tc5h,
+  '6h': tc6h,
+  '7h': tc7h,
+  '8h': tc8h,
+  '9h': tc9h,
+  'Th': tcTh,
+  'Jh': tcJh,
+  'Qh': tcQh,
+  'Kh': tcKh,
+  'Ah': tcAh,
+
+  '2s': tc2s,
+  '3s': tc3s,
+  '4s': tc4s,
+  '5s': tc5s,
+  '6s': tc6s,
+  '7s': tc7s,
+  '8s': tc8s,
+  '9s': tc9s,
+  'Ts': tcTs,
+  'Js': tcJs,
+  'Qs': tcQs,
+  'Ks': tcKs,
+  'As': tcAs,
+}
+
+const cardsTwoColorBF = {
+  '2c': tcbf2c,
+  '3c': tcbf3c,
+  '4c': tcbf4c,
+  '5c': tcbf5c,
+  '6c': tcbf6c,
+  '7c': tcbf7c,
+  '8c': tcbf8c,
+  '9c': tcbf9c,
+  'Tc': tcbfTc,
+  'Jc': tcbfJc,
+  'Qc': tcbfQc,
+  'Kc': tcbfKc,
+  'Ac': tcbfAc,
+
+  '2d': tcbf2d,
+  '3d': tcbf3d,
+  '4d': tcbf4d,
+  '5d': tcbf5d,
+  '6d': tcbf6d,
+  '7d': tcbf7d,
+  '8d': tcbf8d,
+  '9d': tcbf9d,
+  'Td': tcbfTd,
+  'Jd': tcbfJd,
+  'Qd': tcbfQd,
+  'Kd': tcbfKd,
+  'Ad': tcbfAd,
+
+  '2h': tcbf2h,
+  '3h': tcbf3h,
+  '4h': tcbf4h,
+  '5h': tcbf5h,
+  '6h': tcbf6h,
+  '7h': tcbf7h,
+  '8h': tcbf8h,
+  '9h': tcbf9h,
+  'Th': tcbfTh,
+  'Jh': tcbfJh,
+  'Qh': tcbfQh,
+  'Kh': tcbfKh,
+  'Ah': tcbfAh,
+
+  '2s': tcbf2s,
+  '3s': tcbf3s,
+  '4s': tcbf4s,
+  '5s': tcbf5s,
+  '6s': tcbf6s,
+  '7s': tcbf7s,
+  '8s': tcbf8s,
+  '9s': tcbf9s,
+  'Ts': tcbfTs,
+  'Js': tcbfJs,
+  'Qs': tcbfQs,
+  'Ks': tcbfKs,
+  'As': tcbfAs,
+}
+
+const cardsFourColor = {
+  '2c': fc2c,
+  '3c': fc3c,
+  '4c': fc4c,
+  '5c': fc5c,
+  '6c': fc6c,
+  '7c': fc7c,
+  '8c': fc8c,
+  '9c': fc9c,
+  'Tc': fcTc,
+  'Jc': fcJc,
+  'Qc': fcQc,
+  'Kc': fcKc,
+  'Ac': fcAc,
+
+  '2d': fc2d,
+  '3d': fc3d,
+  '4d': fc4d,
+  '5d': fc5d,
+  '6d': fc6d,
+  '7d': fc7d,
+  '8d': fc8d,
+  '9d': fc9d,
+  'Td': fcTd,
+  'Jd': fcJd,
+  'Qd': fcQd,
+  'Kd': fcKd,
+  'Ad': fcAd,
+
+  '2h': fc2h,
+  '3h': fc3h,
+  '4h': fc4h,
+  '5h': fc5h,
+  '6h': fc6h,
+  '7h': fc7h,
+  '8h': fc8h,
+  '9h': fc9h,
+  'Th': fcTh,
+  'Jh': fcJh,
+  'Qh': fcQh,
+  'Kh': fcKh,
+  'Ah': fcAh,
+
+  '2s': fc2s,
+  '3s': fc3s,
+  '4s': fc4s,
+  '5s': fc5s,
+  '6s': fc6s,
+  '7s': fc7s,
+  '8s': fc8s,
+  '9s': fc9s,
+  'Ts': fcTs,
+  'Js': fcJs,
+  'Qs': fcQs,
+  'Ks': fcKs,
+  'As': fcAs,
+}
+
+const cardsFourColorBF = {
+  '2c': fcbf2c,
+  '3c': fcbf3c,
+  '4c': fcbf4c,
+  '5c': fcbf5c,
+  '6c': fcbf6c,
+  '7c': fcbf7c,
+  '8c': fcbf8c,
+  '9c': fcbf9c,
+  'Tc': fcbfTc,
+  'Jc': fcbfJc,
+  'Qc': fcbfQc,
+  'Kc': fcbfKc,
+  'Ac': fcbfAc,
+
+  '2d': fcbf2d,
+  '3d': fcbf3d,
+  '4d': fcbf4d,
+  '5d': fcbf5d,
+  '6d': fcbf6d,
+  '7d': fcbf7d,
+  '8d': fcbf8d,
+  '9d': fcbf9d,
+  'Td': fcbfTd,
+  'Jd': fcbfJd,
+  'Qd': fcbfQd,
+  'Kd': fcbfKd,
+  'Ad': fcbfAd,
+
+  '2h': fcbf2h,
+  '3h': fcbf3h,
+  '4h': fcbf4h,
+  '5h': fcbf5h,
+  '6h': fcbf6h,
+  '7h': fcbf7h,
+  '8h': fcbf8h,
+  '9h': fcbf9h,
+  'Th': fcbfTh,
+  'Jh': fcbfJh,
+  'Qh': fcbfQh,
+  'Kh': fcbfKh,
+  'Ah': fcbfAh,
+
+  '2s': fcbf2s,
+  '3s': fcbf3s,
+  '4s': fcbf4s,
+  '5s': fcbf5s,
+  '6s': fcbf6s,
+  '7s': fcbf7s,
+  '8s': fcbf8s,
+  '9s': fcbf9s,
+  'Ts': fcbfTs,
+  'Js': fcbfJs,
+  'Qs': fcbfQs,
+  'Ks': fcbfKs,
+  'As': fcbfAs,
+}
+
+
+const selectDeck = deckType => {
+  if (deckType === "big-face") {return cardsTwoColorBF}
+  if (deckType === "big-face four-color") {return cardsFourColorBF}
+  if (deckType === "four-color") {return cardsFourColor}
+
+  return cardsTwoColor
+}
+
+function Card(props) {
+  const {card, front = true, back, deckType, height} = props
+  const style = {height}
+  const cards = selectDeck(deckType)
+
+  if (back) {
+    return (
+      <img src={b} className="card" alt="card-back" style={style} />
+    )
+  }
+
+  if (front) {
+    return (
+      <img src={cards[card]} className="card" alt={card} style={style} />
+    )
+  }
+}
+
+export default Card
