75e1dc46a63c1e3c3d186e58ff7c2e29343d8eeb
Shane Krueger <shane@acdmail.com>
2021-11-06 14:48:46 -0400

    Allow specifying custom class and styles

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,18 +3,23 @@ import React from "react"
 import b from "../images/back.svg"
 
 function Card(props) {
-  const {card, front = true, back, Component, height} = props
-  const style = {height}
+  const {card, front = true, back, Component, height, style, className} = props
+  if (style) {
+    if (height !== undefined)
+      style.height = height;
+  }
+  else
+    style = {height};
 
   if (back) {
     return (
-      <img src={b} className="card card__back" alt="card-back" style={style} />
+      <img src={b} className={"card card__back " + (className || "")} alt="card-back" style={style} />
     )
   }
 
   if (front) {
     return (
-      <img src={card} className="card card" alt={card} style={style} />
+      <img src={card} className={"card card__front " + (className || "")} alt={card} style={style} />
     )
   }
 }
