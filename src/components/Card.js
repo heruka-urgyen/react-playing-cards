be0803b442bed4d7ea45bf6088f7a0cbf8c0faba
Shane Krueger <shane@acdmail.com>
2021-11-10 00:17:06 -0500

    Update Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,23 +3,18 @@ import React from "react"
 import b from "../images/back.svg"
 
 function Card(props) {
-  const {card, front = true, back, Component, height, style, className} = props
-  if (style) {
-    if (height !== undefined)
-      style.height = height;
-  }
-  else
-    style = {height};
+  const {card, front = true, back, Component, height, style = {}, className = ""} = props;
+  style.height = height;
 
   if (back) {
     return (
-      <img src={b} className={"card card__back " + (className || "")} alt="card-back" style={style} />
+      <img src={b} className={`react-playing-card react-playing-card__back ${className}`} alt="card-back" style={style} />
     )
   }
 
   if (front) {
     return (
-      <img src={card} className={"card card__front " + (className || "")} alt={card} style={style} />
+      <img src={card} className={`react-playing-card react-playing-card__back ${className}`} alt={card} style={style} />
     )
   }
 }
